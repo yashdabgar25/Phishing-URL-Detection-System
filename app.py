33f0a8f9333a3f6061a0b97edca37235c2aa0519
yashdabgar25 <yashdabgar4251@gmail.com>
2026-09-12 12:40:57 +0530

    initial project setup

---
+++ app.py
@@ -0,0 +1,205 @@
+from flask import Flask, render_template, request, jsonify
+from urllib.parse import urlparse 
+import re
+import sqlite3
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect("phishing_history.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT NOT NULL,
+        result TEXT NOT NULL,
+        risk_score INTEGER NOT NULL,
+        risk_level TEXT NOT NULL,
+        warnings TEXT,
+        scan_time TEXT NOT NULL
+    )
+""")
+    conn.commit()
+    conn.close()
+
+def detect_phishing(url):
+    score = 0
+    warnings = []
+
+    url_lower = url.lower()
+
+    # Check URL length
+    if len(url) > 75:
+        score += 1
+        warnings.append("URL is unusually long.")
+
+    # Check HTTPS
+    if not url_lower.startswith("https://"):
+        score += 1
+        warnings.append("URL does not use HTTPS.")
+
+    # Check suspicious symbols
+    if "@" in url:
+        score += 2
+        warnings.append("URL contains an @ symbol.")
+
+    try:
+        hostname = urlparse(url).hostname
+
+        if hostname:
+            ip_pattern = r"^\d{1,3}(\.\d{1,3}){3}$"
+
+            if re.match(ip_pattern, hostname):
+                score += 2
+                warnings.append("URL uses an IP address instead of a domain name.")
+    except Exception:
+        score += 1
+        warnings.append("URL format could not be analyzed.")
+
+    # Check suspicious words
+    suspicious_words = [
+        "login",
+        "verify",
+        "verification",
+        "account",
+        "secure",
+        "update",
+        "password",
+        "confirm",
+        "bank",
+        "free",
+        "signin"
+    ]
+
+    found_words = []
+
+    for word in suspicious_words:
+        if word in url_lower:
+            found_words.append(word)
+
+    if len(found_words) >= 2:
+        score += 2
+        warnings.append("URL contains multiple suspicious keywords.")
+
+    # Check number of subdomains
+    try:
+        hostname = urlparse(url).hostname
+
+        if hostname:
+            parts = hostname.split(".")
+
+            if len(parts) > 4:
+                score += 1
+                warnings.append("URL contains an unusual number of subdomains.")
+
+    except Exception:
+        pass
+
+    if url_lower.count("-") >= 3:
+        score += 1
+        warnings.append("URL contains multiple hyphens.")
+
+    # Final result
+    if score >= 5:
+        result = "Potentially Phishing"
+    elif score >= 2:
+        result = "Suspicious"
+    else:
+        result = "Likely Safe"
+
+    return result, score, warnings
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/check-url", methods=["POST"])
+def check_url():
+
+    data = request.get_json()
+    url = data.get("url", "").strip()
+
+    if not url:
+        return jsonify({
+            "status": "error",
+            "message": "Please enter a URL."
+        })
+    
+    result, score, warnings = detect_phishing(url)
+
+    if score <= 1:
+        risk_level = "Low Risk"
+    elif score <= 4:
+        risk_level = "Medium Risk"
+    else:
+        risk_level = "High Risk"
+
+
+    conn = sqlite3.connect("phishing_history.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO history (url, result, risk_score, risk_level, warnings, scan_time) VALUES (?, ?, ?, ?, ?, ?)
+""",(
+    url,
+    result,
+    score,
+    risk_level,
+    json.dumps(warnings),
+    datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+))
+    conn.commit()
+    conn.close()
+
+    
+    return jsonify({
+        "status": "success",
+        "message": result,
+        "url": url,
+        "risk_score": score,
+        "risk_level": risk_level,
+        "warnings": warnings
+    })
+@app.route("/history-page")
+def history_page():
+    return render_template("history.html")
+
+@app.route("/history", methods=["GET"])
+def history():
+
+    conn = sqlite3.connect("phishing_history.db")
+    conn.row_factory = sqlite3.Row
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT * FROM history 
+        ORDER BY id DESC
+""")
+
+    records = cursor.fetchall()
+
+    conn.close()
+
+    history_data = []
+
+    for record in records:
+        history_data.append({
+            "id":record["id"],
+            "url":record["url"],
+            "result":record["result"],
+            "risk_score":record["risk_score"],
+            "risk_level":record["risk_level"],
+            "warnings":json.loads(record["warnings"]) if record["warnings"]else[],
+            "scan_time":record["scan_time"],
+        })
+
+    return jsonify(history_data)
+
+
+if __name__=="__main__":
+    init_db()
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,124 @@
+const checkButton = document.getElementById("checkButton");
+const urlInput = document.getElementById("urlInput");
+const result = document.getElementById("result");
+checkButton.addEventListener("click", async function()
+{
+    const url = urlInput.value.trim();
+    if (url === ""){
+        result.innerHTML = `
+            <div class="result-card error-card">
+                <h2> ⚠️ Please enter a URL</h2>
+                <p>Enter a website URL to continue.</p>
+            </div>
+        `;
+        return;
+    }
+
+    result.innerHTML = `
+            <div class="result-card loading-card">
+                <h2> 🔍 Checking URL...</h2>
+                <p>Please wait while we analyze the URL.</p>
+            </div>
+         `;
+    try {
+        const response = await fetch("/check-url", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                url: url
+            })
+        });
+        const data = await response.json();
+
+        if (data.status === "success"){
+
+            let resultClass = "";
+            let icon = "";
+
+            if (data.message === "Likely Safe"){
+                resultClass = "safe-card";
+                icon = "";
+            }
+
+            if (data.message === "Likely Safe") {
+                resultClass = "safe-card";
+                icon = "🟢";
+            }
+            else if (data.message === "Suspicious") {
+                resultClass = "warning-card";
+                icon = "🟡";
+            }
+            else {
+                resultClass = "danger-card";
+                icon = "🔴";
+            }
+            let warningsHTML = "";
+
+            if (data.warnings.length > 0){
+            warningsHTML = `
+           <div class = "warnings">
+           <h3 ⚠️ Warnings</h3>
+           <ul>
+                ${data.warnings.map(warning => `<li>${warning}</li>`).join("")}
+            </ul>
+            </div>
+            `;
+            }else {
+                warningsHTML = `
+                    <div class = "warnings"
+                        <h3> ✓ No major warnings detected</h3>
+                    </div>
+                    `;
+            }
+                    result.innerHTML = 
+                    `<div class = "result-card ${resultClass}">
+
+                        <div class = "result-header">
+                        <span class = "result-icon">${icon}</span>
+                            <div>
+                                <h2>${data.message}</h2>
+                                <p>URL analysis completed</p>
+                            </div>
+                        </div>
+
+                        <div class= "url-box">
+                            <strong> Scanned URL: </strong>
+                            <p>${data.url}</p>
+                        </div>
+
+                        <div class = "risk-score">
+                            <strong>Risk Score:</strong>
+                            <span>${data.risk_score}/10</span>
+                        </div>
+
+                        <div class = "risk_level">
+                            <strong>Risk Level:</strong>
+                            <span>${data.risk_level}</span>
+                        </div>
+
+                        ${warningsHTML}
+
+                    </div>
+                    `;
+        }
+        else {
+            result.innerHTML = `
+            <div class = "result-card error-card">
+                <h2> ❌ Error</h2>
+                <p>${data.message}</p>
+            </div>
+            `;
+        }
+    }catch (err) {
+        result.innerHTML = `
+        <div class = "result-card error-card">
+            <h2> ❌ Something went wrong</h2>
+            <p>Please try again.</p>
+        </div>
+        `;
+
+        console.error(err);
+    }
+});
